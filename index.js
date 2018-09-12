7f885afd7263aaabd33b5184b43edc4cb847e781
Eetu Blomqvist <eetu.blomqvist@reaktor.com>
2018-09-12 00:39:06 -0400

    Add some caching

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import express from 'express'
 import Mta from 'mta-gtfs'
 import 'dotenv/config'
+import mcache from 'memory-cache'
 
 const app = express()
 const mta = new Mta({
     key: process.env.MTA_API_KEY
 })
 
-app.get('/schedule/:feedId/stop/:stopId/direction/:direction', (req, res, next) => {
+const cache = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.url
+        let cachedBody = mcache.get(key)
+        if(cachedBody) {
+            res.send(cachedBody)
+            return
+        }
+        else {
+            res.sendResponse = res.send
+            res.send = (body) => {
+                mcache.put(key, body, duration * 1000)
+                res.sendResponse(body)
+            }
+            next()
+        }
+    }
+}
+
+
+app.get('/schedule/:feedId/stop/:stopId/direction/:direction', cache(30), (req, res, next) => {
     const stopId = req.params.stopId
     const feedId = req.params.feedId
     const direction = req.params.direction
